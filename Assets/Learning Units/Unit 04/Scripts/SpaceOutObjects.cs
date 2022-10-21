9cf6408c671630921832ccea019888d16eee40c1
masonwilson <mew8wjn@virginia.edu>
2022-10-21 09:11:48 -0400

    LU 4

--- Assets/Learning Units/Unit 04/Scripts/SpaceOutObjects.cs
+++ Assets/Learning Units/Unit 04/Scripts/SpaceOutObjects.cs
@@ -5,17 +5,19 @@ using UnityEngine;
 public class SpaceOutObjects : MonoBehaviour
 {
     // DIRECTIONS: The goal of this Unit is to get the objects to space out evenly
+    public List<GameObject> ObjectsToMove { get; set; }
+    public List<GameObject> objectsToSpaceOut { get; set; }
 
     // Start is called before the first frame update
     void Start()
     {
         // DIRECTION: Uncomment the lines below, then fix them...
-        //GameObject objectsToSpaceOut = GetComponent<MoveObjects>().ObjectsToMove;
+        objectsToSpaceOut = GetComponent<MoveObjects>().ObjectsToMove;
         
-        //for (something; something something objectsToSpaceOut.Count; something)
-        //{
-        //    somethingsomething.transform.position = new Vector3(something * 2f,0,0);
-        //}
+        for (int i = 0; i < objectsToSpaceOut.Count; i++)
+        {
+            objectsToSpaceOut[i].transform.position = new Vector3(i * 3f,0,0);
+        }
 
         GetComponent<MoveObjects>().enabled = false; // Keep this line. We've got these objects from this 
         // Component, and we don't need it anymore
